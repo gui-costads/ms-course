969ca911cbf408c83724b30679a98821a47c54fc
Guilherme Costa <guilhermecosta.ds1@gmail.com>
2023-04-15 16:24:33 -0300

    Add Repository of class worker

---
+++ hr-worker/src/main/java/com/gc/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,9 @@
+package com.gc.hrworker.repositories;
+
+import com.gc.hrworker.entities.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+}
