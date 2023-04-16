68939319887fbb517e36db4fd07336f5e0314f7f
Guilherme Costa <guilhermecosta.ds1@gmail.com>
2023-04-15 23:32:36 -0300

    Add Rest Template and configurations

--- hr-payroll/src/main/java/com/gc/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/gc/hrpayroll/services/PaymentService.java
@@ -1,12 +1,30 @@
 package com.gc.hrpayroll.services;
 
 import com.gc.hrpayroll.entities.Payment;
+import com.gc.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
+    @Value("${hr-worker.host}")
+    private String hrWorkerHost;
+    private RestTemplate restTemplate;
+
+    public PaymentService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     public Payment getPayment(Long workerId, Integer days){
-        return new Payment("mario", 200.0, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", ""+workerId);
+
+
+        Worker worker = restTemplate.getForObject(hrWorkerHost + "/workers/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
