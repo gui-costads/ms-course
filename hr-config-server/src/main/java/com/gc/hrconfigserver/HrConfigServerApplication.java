8e39289ddb22dce1a7d2b5b447e5b9734814d4d2
Guilherme Costa <guilhermecosta.ds1@gmail.com>
2023-04-20 22:49:43 -0300

    Configuring private git repository

--- hr-config-server/src/main/java/com/gc/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/gc/hrconfigserver/HrConfigServerApplication.java
@@ -1,8 +1,9 @@
 package com.gc.hrconfigserver;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
@@ -12,5 +13,4 @@ public class HrConfigServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
 	}
-
 }
